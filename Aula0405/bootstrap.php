bb9c927b24a435439a8c9b2affb960f104bf5bed
henriquesantos21 <100103787+henriquesantos21@users.noreply.github.com>
2022-05-04 20:49:40 -0300

    Update bootstrap.php

--- Aula0405/bootstrap.php
+++ Aula0405/bootstrap.php
@@ -1,24 +1,28 @@
 <?php
 
-    require_once __DIR__."/vendor/autoload.php";
+require_once __DIR__."/vendor/autoload.php";
 
-    //Aqui a gente recupera oque o usuário digitou e qual metodo HTTP ele utilizou
-    $merthod = $_SERVER["REQUEST_METHOD"];
-    $path = $_SERVER["PATH_INFO"];
+//Aqui a gente recupera oque o usuário digitou e qual metodo HTTP ele utilizou
+$merthod = $_SERVER["REQUEST_METHOD"];
+$path = $_SERVER["PATH_INFO"];
 
-    //Instanciar classe Router
-    $router = new \Aluno\Aula0405\Router($method, $path);
+//Instanciar classe Router
+$router = new \Aluno\Aula0405\Router($method, $path);
 
-    //ADICIONAR AS ROTAS VÁLIDAS ABAIXO
+//ADICIONAR AS ROTAS VÁLIDAS ABAIXO
 
+$router->get('/ola-mundo', function(){
+    return "Olá Mundo!";
+});
 
-
-    //ADICIONAR AS ROTAS VÁLIDAS ACIMA
+//ADICIONAR AS ROTAS VÁLIDAS ACIMA
 
     $result = $router->handler();
 
-    if (!$result){
-        http_response_code(404);
-        echo "Página nâo encontrada";
-        die();
-    }
+if (!$result){
+    http_response_code(404);
+    echo "Página nâo encontrada";
+    die();
+}
+
+echo $result($router->getParams());
