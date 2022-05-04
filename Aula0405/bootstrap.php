6583253d642433cad192d1066918560253af7139
henriquesantos21 <100103787+henriquesantos21@users.noreply.github.com>
2022-05-04 20:33:21 -0300

    henriquesantos21

---
+++ Aula0405/bootstrap.php
@@ -0,0 +1,24 @@
+<?php
+
+    require_once __DIR__."/vendor/autoload.php";
+
+    //Aqui a gente recupera oque o usuário digitou e qual metodo HTTP ele utilizou
+    $merthod = $_SERVER["REQUEST_METHOD"];
+    $path = $_SERVER["PATH_INFO"];
+
+    //Instanciar classe Router
+    $router = new \Aluno\Aula0405\Router($method, $path);
+
+    //ADICIONAR AS ROTAS VÁLIDAS ABAIXO
+
+
+
+    //ADICIONAR AS ROTAS VÁLIDAS ACIMA
+
+    $result = $router->handler();
+
+    if (!$result){
+        http_response_code(404);
+        echo "Página nâo encontrada";
+        die();
+    }
