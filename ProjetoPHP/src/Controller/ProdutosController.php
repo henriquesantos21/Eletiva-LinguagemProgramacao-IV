a2cb26327488afedc1dbc174ee66c14721584a0f
henriquesantos21 <100103787+henriquesantos21@users.noreply.github.com>
2022-06-04 04:45:04 -0300

    produto

--- ProjetoPHP/src/Controller/ProdutosController.php
+++ ProjetoPHP/src/Controller/ProdutosController.php
@@ -1,4 +1,17 @@
 <?php
 namespace Aluno\ProjetoPHP\Controller;
 
-use Aluno\ProjetoPHP\Model\Entity\Produtos;
+use Aluno\ProjetoPhp\Model\DAO\ProdutosDAO;
+use Aluno\ProjetoPhp\Model\Entity\Produtos;
+
+class ProdutosController{
+    public static function abrirFormularioInserir(){
+        require_once "../src/View/inserir_produto.php";
+    }
+
+    public static function abrirFormularioAlterar($params){
+        $dao = new ProdutosDAO();
+        $resultado = $dao->consultarPorCodigo($params[1]);
+        require_once "../src/View/alterar_produto.php"
+    }
+}
